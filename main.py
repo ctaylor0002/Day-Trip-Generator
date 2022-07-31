3a06ae60d32a23d9091584b0d9121b930245a173
ctaylor0002 <charles4312@comcast.net>
2022-07-31 17:03:06 -0400

    Added ability to confirm trip or deny and restart the process. Also fixed a bug that caused infinite looping when yes/no is not typed into input fields.

--- main.py
+++ main.py
@@ -26,6 +26,7 @@
 #startingLocation = "Springfield MA" This is probably unneccessary
 
 import random
+from tkinter.messagebox import YES
 locations = ["Boston", "New York City", "Pittsburgh", "Washington DC", "Salem", "Hartford", "Portland"]
 transportation = ["Train", "Plane", "Rental car", "Bus", "Uber"]
 
@@ -53,8 +54,12 @@ def pickedLocation():
     acceptableLocation = False
     locationIndex = random.randrange(0,len(locations),1)
     location = locations[locationIndex]
-    question = input(f"Your picked location for today is {location}. Is it okay? yes/no ")
+    question = ''
+
     while acceptableLocation == False:
+        if question == '':
+            question = input(f"Your picked location for today is {location}. Is it okay? yes/no ")
+        
         if question == 'yes':
             print(f"Awesome! Hope you have fun at {location}!")
             acceptableLocation == True
@@ -67,15 +72,17 @@ def pickedLocation():
             location = locations[locationIndex]
             question = input(f"How about {location}? yes/no ")
         else:
-            print('Invalid response. Try again.')
+            question = input('Invalid response. Try again. yes/no ')
 
 def pickedTransportation():
     acceptableTransportation = False
     transportationIndex = random.randrange(0,len(transportation),1)
     transportationChk = transportation[transportationIndex]
-    question = input(f"Your picked method of transportation for today is {transportationChk}. Is this okay? yes/no ")
-
+    question = ''
     while acceptableTransportation == False:
+        if question == '':
+            question = input(f"Your picked method of transportation for today is {transportationChk}. Is this okay? yes/no ")
+        
         if question == 'yes':
             print(f"Fantastic! Enjoy your {transportationChk} ride!")
             return transportationChk
@@ -85,7 +92,7 @@ def pickedTransportation():
             transportationChk = transportation[transportationIndex]
             question = input(f"How about taking a {transportationChk}? yes/no ")
         else:
-             print('Invalid respons. Try again.')
+            question = input('Invalid response. Try again. yes/no ')
 
 
 def pickedEntertainment(tripLocation):
@@ -93,11 +100,13 @@ def pickedEntertainment(tripLocation):
     entertainmentOptions = entertainment[srchIndex]
     entertainmentOption = random.randrange(0,len(entertainmentOptions),1)
     theEntertainmentOption = entertainmentOptions[entertainmentOption]
-
+    question = ''
     acceptableEntertainment = False
-    question = input(f"Your picked entertainment for the day is touring {theEntertainmentOption}. Is this okay? yes/no ")
-
+   
     while acceptableEntertainment == False:
+        if question == '':
+            question = input(f"Your picked entertainment for the day is touring {theEntertainmentOption}. Is this okay? yes/no ")
+        
         if question == 'yes':
             print(f"Perfect! Enjoy touring {theEntertainmentOption}!")
             return theEntertainmentOption
@@ -107,7 +116,7 @@ def pickedEntertainment(tripLocation):
             theEntertainmentOption = entertainmentOptions[srchIndex]
             question = input(f"How about touring {theEntertainmentOption}? yes/no ")
         else:
-            print("Invalid Response. Try again,")
+            question = input('Invalid response. Try again. yes/no ')
 
         
 def pickedRestaurant(tripLocation):
@@ -115,11 +124,13 @@ def pickedRestaurant(tripLocation):
     restaurantOptions = restaurants[srchIndex]
     restaurantOption = random.randrange(0,len(restaurantOptions),1)
     theRestaurantOption = restaurantOptions[restaurantOption]
-
+    question = ''
     acceptableRestaurant = False
-    question = input(f"Your picked restaurant for the day is {theRestaurantOption}. Is this okay? yes/no ")
-
+    
     while acceptableRestaurant == False:
+        if question == '':
+            question = input(f"Your picked restaurant for the day is {theRestaurantOption}. Is this okay? yes/no ")
+        
         if question == 'yes':
             print(f"Perfect! Enjoy eating at {theRestaurantOption}!")
             return theRestaurantOption
@@ -131,9 +142,46 @@ def pickedRestaurant(tripLocation):
         else:
             print("Invalid Response. Try again,")
 
-        
+def tripGeneration():
+    locations = ["Boston", "New York City", "Pittsburgh", "Washington DC", "Salem", "Hartford", "Portland"]
+    transportation = ["Train", "Plane", "Rental car", "Bus", "Uber"]
 
-tripLocation = pickedLocation()
-tripTransportation = pickedTransportation()
-tripEntertainment = pickedEntertainment(tripLocation)
-tripRestaurant = pickedRestaurant(tripLocation)
+    #The list of restaurants will have a list of lists
+    #I will add 3 locations to each list. If the user declines all three I will have a basic Fast Food list to accomadate
+    restaurants = [["Maggiano's Little Italy", "Bostonia Public House", "Boston Sail Loft"],
+                    ["STK Steakhouse Downtown NYC", "Boucherie Union Square", "Le Coucou"],
+                    ["Morton's The Steakhouse", "Eddie V's Prime Seafood", "The Capital Grille"],
+                    ["Benihana", "Founding Farmers DC", "Estadio"],
+                    ["Jade's Restaurant","Bambolina", "Adriatc Restaurant"],
+                    ["Bear's Smokehouse Barbecue", "Black-Eyed Sally's Southern Kitchen and Bar", "Vaughan's Public House"],
+                    ["Rick's Cafe", "Duckfat","Hot Suppa"]
+    ]
+
+    entertainment = [["New England Aquarium", "Museum of Fine Arts", "Fenway Park"],
+                        ["Central Park", "The Metropolitan Museum of Art", "Statue of Liberty"],
+                        ["The Andy Warhol Museum", "Phipps Conservatory and Botanical Gardens", "Pittsburgh Zoo and PPG Aquarium"],
+                        ["The White House", "Library of Congress","United States Capitol"],
+                        ["Salem Witch Museum", "Salem Witch Trials Memorial","Salem Maritime National Historic Site"],
+                        ["Connecticut Science Center", "The Mark Twain House and Museum", "Wadsworth Athenum Museum of Art"],
+                        ["Portland Head Light", "Portland Museum of Art", "Peaks Island"]
+    ]
+    tripLocation = pickedLocation()
+    tripTransportation = pickedTransportation()
+    tripEntertainment = pickedEntertainment(tripLocation)
+    tripRestaurant = pickedRestaurant(tripLocation)
+    returnData = (tripLocation,tripTransportation,tripEntertainment,tripRestaurant)
+    return returnData
+
+generatedTrip = tripGeneration()
+confirmation = False
+print(f"The trip we have generated for you is as follows:\n Location: {generatedTrip[0]}\n Transportation: {generatedTrip[1]}\n Entertainment: {generatedTrip[2]}\n Restaurant: {generatedTrip[3]}")
+
+while confirmation == False:
+    question = input("Is this trip to your liking? yes/no ")
+    if question == 'yes':
+        print("Have fun at your trip today!!!")
+        confirmation = True
+    elif question == 'no':
+        generatedTrip = tripGeneration()
+
+
